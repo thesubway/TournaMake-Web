38d47a509a900606948f9b85a0c9525035fcd7b4
Daniel Hoang <thesubway@gmail.com>
2016-03-21 14:22:21 -0700

    Remove tournament id, add format, migrate

--- db/migrate/20160321212100_create_tournaments.rb
+++ db/migrate/20160321212100_create_tournaments.rb
@@ -2,7 +2,7 @@ class CreateTournaments < ActiveRecord::Migration
   def change
     create_table :tournaments do |t|
       t.string :name
-      t.integer :id
+      t.string :format
 
       t.timestamps null: false
     end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160320021922) do
+ActiveRecord::Schema.define(version: 20160321212100) do
+
+  create_table "tournaments", force: :cascade do |t|
+    t.string   "name"
+    t.string   "format"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
