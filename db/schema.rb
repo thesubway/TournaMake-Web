72dde97330f23c205c23656378e04370243fa951
Daniel Hoang <thesubway@gmail.com>
2016-03-30 09:11:02 -0700

    Migrate and set up Entrant

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160325161728) do
+ActiveRecord::Schema.define(version: 20160329015733) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "entrants", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "matches", force: :cascade do |t|
     t.string   "leftId"
--- test/models/entrant_test.rb
+++ test/models/entrant_test.rb
@@ -1,7 +1,11 @@
 require 'test_helper'
 
 class EntrantTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+      @Entrant = entrants(:one)
+  end
+
+  test "entrant is valid" do
+      assert @Entrant.valid?
+  end
 end
