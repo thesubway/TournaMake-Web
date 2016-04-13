31d31b756921985b60f1d94a6a1f6122c6ce057a
Daniel Hoang <thesubway@gmail.com>
2016-04-13 09:29:23 -0700

    Validate letter existence and length

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,2 +1,3 @@
 class Group < ActiveRecord::Base
+    validates :letter, presence:true, length: {maximum: 1}
 end
--- test/models/group_test.rb
+++ test/models/group_test.rb
@@ -8,4 +8,14 @@ class GroupTest < ActiveSupport::TestCase
   test "Group is valid" do
       assert @Group.valid?
   end
+
+  test "letter should be present" do
+      @Group.letter = "     "
+      assert_not @Group.valid?
+  end
+
+  test "letter should have length 1" do
+      @Group.letter = "AB"
+      assert_not @Group.valid?
+  end
 end
