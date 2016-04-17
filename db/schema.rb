5a2ecaf63361640c8e936e3061aa912d5d3dc9b5
Daniel Hoang <thesubway@gmail.com>
2016-04-17 16:25:08 -0700

    Add correct tournament-group ref

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160414185850) do
+ActiveRecord::Schema.define(version: 20160417232300) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -63,12 +63,14 @@ ActiveRecord::Schema.define(version: 20160414185850) do
   create_table "tournaments", force: :cascade do |t|
     t.string   "name"
     t.string   "format"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
     t.integer  "entrant_id"
+    t.integer  "tournament_id"
   end
 
   add_index "tournaments", ["entrant_id"], name: "index_tournaments_on_entrant_id", using: :btree
+  add_index "tournaments", ["tournament_id"], name: "index_tournaments_on_tournament_id", using: :btree
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -91,4 +93,5 @@ ActiveRecord::Schema.define(version: 20160414185850) do
   add_foreign_key "entrants", "tournaments"
   add_foreign_key "groups", "tournaments"
   add_foreign_key "tournaments", "entrants"
+  add_foreign_key "tournaments", "tournaments"
 end
