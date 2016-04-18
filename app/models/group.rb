04bc83afc617ed63b1728ecc794f0c3327c4eae3
Daniel Hoang <thesubway@gmail.com>
2016-04-18 09:20:58 -0700

    Set correct has_many for tournament-groups

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,3 +1,4 @@
 class Group < ActiveRecord::Base
+    belongs_to :tournament
     validates :letter, presence:true, length: {maximum: 1}
 end
--- app/models/tournament.rb
+++ app/models/tournament.rb
@@ -1,4 +1,5 @@
 class Tournament < ActiveRecord::Base
+    has_many :groups
     has_many :entrants
     validates :name, presence: true
 end
