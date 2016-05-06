d02b2af303f5fb3e9ed08530080a1976d9cc69d3
Daniel Hoang <thesubway@gmail.com>
2016-05-06 16:56:54 -0700

    Set Tournament params, fill new def

--- app/controllers/tournaments_controller.rb
+++ app/controllers/tournaments_controller.rb
@@ -1,4 +1,11 @@
 class TournamentsController < ApplicationController
   def new
+    tournament = Tournament.new(tournament_params)
+    tournament.save
+  end
+
+  private
+  def tournament_params
+    params.require(:tournament).permit(:name)
   end
 end
