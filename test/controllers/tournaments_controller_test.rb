3a0268316a2692886c3286ffc201d3718d6d9e19
Daniel Hoang <thesubway@gmail.com>
2016-05-11 18:42:23 -0700

    Add setup to tournaments controller test

--- test/controllers/tournaments_controller_test.rb
+++ test/controllers/tournaments_controller_test.rb
@@ -1,6 +1,10 @@
 require 'test_helper'
 
 class TournamentsControllerTest < ActionController::TestCase
+  def setup
+    @tournament = tournaments(:one)
+  end
+
   test "should get new" do
     get :new
     assert_response :success
