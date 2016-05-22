f057e2cf1f37b572a7af1a65c9069948f88e982a
Daniel Hoang <thesubway@gmail.com>
2016-05-22 00:33:54 -0700

    Add default route, add react

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
   devise_for :users
 
   get 'tournaments' => 'tournaments#index'
+
+  root 'tournaments#new'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
