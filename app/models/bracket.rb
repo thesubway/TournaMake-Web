8245a480d61f9584d9abbe3f63924bdc2d916a90
Daniel Hoang <thesubway@gmail.com>
2016-05-29 16:22:00 -0700

    display tournament ids from bracket

--- app/models/bracket.rb
+++ app/models/bracket.rb
@@ -1,6 +1,6 @@
 class Bracket < ActiveRecord::Base
     validates :isStarted, presence:true
     belongs_to :tournament
-    has_many :matches
-    has_many :bracket_slots
+    # has_many :matches
+    # has_many :bracket_slots
 end
